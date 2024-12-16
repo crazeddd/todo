f5d7c2c3fba2129633d3d52e82a967090c11a12f
Crazed <cranninja@gmail.com>
2024-12-16 18:30:18 +0000

    Small update to functions

--- src/handleTodo.ts
+++ src/handleTodo.ts
@@ -5,44 +5,61 @@ const handleTodo = () => {
 
     const createItem = (name: string, date: Date) => {
         try {
-            if (!name || !date) return false;
+            if (!name || !date) return Error;
 
-            const items: { name: string; date: Date }[] = JSON.parse(localStorage.getItem("items") || "[]");
+            const cachedItems: { name: string; date: Date }[] = JSON.parse(localStorage.getItem("tasks") || "[]");
             const data = { name, date };
 
-            items.push(data);
+            cachedItems.push(data);
 
-            localStorage.setItem("items", JSON.stringify(items));
-            setItems(items);
+            localStorage.setItem("tasks", JSON.stringify(cachedItems));
+            setItems(cachedItems);
 
-            console.log(`Created new list item (${name})!`)
+            console.log(`Created new list item (${name})`)
             return true;
         } catch (err) {
-            console.warn("Failed to create new list item", err);
-            return false;
+            console.error("Failed to create new list item", err);
         }
     }
 
     const removeItem = (index: number) => {
-        const items = JSON.parse(localStorage.getItem("items") || "[]");
-        console.log(`Removing ${items[index].name}...`);
+        try {
+            const cachedItems: { name: string; date: Date }[] = JSON.parse(localStorage.getItem("tasks") || "[]");
 
-        items.splice(index, 1);
-        localStorage.setItem("items", JSON.stringify(items));
-        setItems(items);
+            console.log(`Removing ${cachedItems[index].name}...`);
+            cachedItems.splice(index, 1);
+            localStorage.setItem("tasks", JSON.stringify(cachedItems));
+
+            setItems(cachedItems);
+        } catch (err) {
+            console.error("Failed to delete item!", err);
+        }
     }
 
-    /*const updateItem = () => {
-
-    }*/
+    const updateItem = (index: number, name: string, date: Date) => { /* Might not be necessary tbh */
+        try {
+            const cachedItems: { name: string; date: Date }[] = JSON.parse(localStorage.getItem("tasks") || "[]");
+            const data = { name, date };
+            cachedItems[index] = data;
+            console.log(`Updated ${name} to ${cachedItems[index].name}`)
+        } catch (err) {
+            console.error(`Failed to update item ${name}`)
+        }
+    }
 
     const removeAllItems = () => {
-        localStorage.clear();
-        setItems([]);
+        try {
+            localStorage.clear();
+            setItems([]);
+            console.log("Cleared local storage")
+        } catch (err) {
+            console.error("Failed to clear local storage", err);
+        }
+
     }
 
     const getItems = () => {
-        setItems(JSON.parse(localStorage.getItem("items") || "[]"));
+        setItems(JSON.parse(localStorage.getItem("tasks") || "[]"));
     }
 
     return { createItem, removeItem, removeAllItems, getItems, items };
